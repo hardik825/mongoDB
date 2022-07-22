0fec2dd68656255c2546d8dd30681dfd92c675b0
Hardik Chauhan <hardikchauhan442@gmail.com>
2022-07-22 17:32:56 +0530

    f

--- resetApi/src/app.js
+++ resetApi/src/app.js
@@ -6,11 +6,13 @@ const app = express();
 const port = process.env.PORT || 3003;
 
 app.use(express.json());
+
 // app.get("/",(req,res)=>{
 //     res.send("Welcome to home page.");
 // })
 
 // /creat a new student
+
 // app.post("/students", (req, res) => {
 //   console.log(req.body);
 //   const user = new Student(req.body);
@@ -34,6 +36,51 @@ app.post("/students", async (req, res) => {
     res.status(400).send(e);
   }
 });
+
+//get student
+
+app.get("/students", async (req, res) => {
+  try {
+    const studentsData = await Student.find();
+    res.status(200).send(studentsData);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//get students by id
+
+app.get("/students/:id", async (req, res) => {
+  try {
+    let _id = req.params.id;
+    const studentData = await Student.findById(_id);
+    if (!studentData) {
+      res.status(404).send();
+    } else {
+      res.send(studentData);
+    }
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+
+//update the students by it id
+
+app.patch("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const UapdateStudent = await Student.findByIdAndUpdate(_id, req.body,{
+      new:true
+    });
+    res.send(UapdateStudent);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+
+// listen
 
 app.listen(port, () => {
   console.log(`connect the port : ${port}....`);
